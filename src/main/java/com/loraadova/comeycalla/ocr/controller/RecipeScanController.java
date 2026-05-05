aee97b2fa8376e5480d5d0d3b10f492948aaefaf
l.adova <l.adova@kamasys.de>
2026-05-05 18:40:23 +0200

    - scan recipe para una imagen y para varias

--- src/main/java/com/loraadova/comeycalla/ocr/controller/RecipeScanController.java
+++ src/main/java/com/loraadova/comeycalla/ocr/controller/RecipeScanController.java
@@ -2,7 +2,6 @@ package com.loraadova.comeycalla.ocr.controller;
 
 import com.loraadova.comeycalla.ocr.RecipeScanService;
 import com.loraadova.comeycalla.ocr.dto.RecipeScanResponseDto;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,15 +13,18 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:4200")
 public class RecipeScanController {
 
-    @Autowired
-    private RecipeScanService recipeScanService;
+    private final RecipeScanService recipeScanService;
+
+    public RecipeScanController(RecipeScanService recipeScanService) {
+        this.recipeScanService = recipeScanService;
+    }
 
     @PostMapping("/scan")
     public ResponseEntity<RecipeScanResponseDto> scanRecipeImages(
             @RequestParam("images") List<MultipartFile> images,
-            @RequestParam(value = "sections", required = false) List<String> sections
+            @RequestParam("sections") List<String> sections
     ) {
-        if (images.size() > 1 && (sections == null || sections.size() != images.size())) {
+        if (images == null || images.isEmpty()) {
             return ResponseEntity.badRequest().build();
         }
 
